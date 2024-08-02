8696d03d773ad4e8ca8c077bfecbed9b02413e25
Sumin Oh <kurkmin@gmail.com>
2024-08-02 21:50:53 +0900

    week 2 added

--- exercise/W02Exercises/W02Exercise2-Common-Errors/W02Exercise2h.java
+++ exercise/W02Exercises/W02Exercise2-Common-Errors/W02Exercise2h.java
@@ -5,7 +5,7 @@
 //     }
 // }
 
-// + in println argument is used as a string concatenation in Java 
+// + sign in println argument is used as a string concatenation in Java 
 // so it cannot be used with - sign, which is used to do with arithmetic operation
 // use round brackets to avoid the problem 
 class W02Exercise2h {
