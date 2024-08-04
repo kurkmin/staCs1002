baf10956409261b8d0e669e6cc862da08fed72bd
Sumin Oh <kurkmin@gmail.com>
2024-08-05 01:03:11 +0900

    week 6 exercises done

---
+++ exercise/W06Exercises/src/Greeter.java
@@ -0,0 +1,18 @@
+public class Greeter {
+    public Greeter() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("hello!");
+    }
+
+    public static void main(String[] args) {
+        String message = "hellllloooo";
+        String otherMessage = message.toUpperCase();
+        System.out.println(message);
+
+        Greeter greet = new Greeter();
+        greet.sayHello();
+    }
+}
---
+++ exercise/W06Exercises/src/W06Exercises.java
@@ -0,0 +1,10 @@
+/**
+ * W06Exercises
+ */
+
+public class W06Exercises {
+    public static void main(String[] args) {
+        String message = "great";
+        System.out.println(message);
+    }
+}
