721c099d1573425c8fcf6d4a38b9ff5a3f4832a7
Sumin Oh <kurkmin@gmail.com>
2024-08-05 14:42:48 +0900

    week 5 exercises done

--- exercise/W05Exercises/W05Exercise1.java
+++ exercise/W05Exercises/W05Exercise1.java
@@ -5,37 +5,64 @@ public class W05Exercise1 {
     Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args) {
-    
+
         W05Exercise1 exercises = new W05Exercise1();
-        
+
         exercises.runExercises();
     }
-    
+
     public void runExercises() {
 
         System.out.println("Enter exercise number:");
         int number = reader.nextInt();
-        
-        switch(number) {
-        	case 1: exercise1(); break;
-            case 2: exercise2(); break;
-            case 3: exercise3(); break;
-            case 4: exercise4(); break; 
-            case 5: exercise5(); break;
-            case 6: exercise6(); break; 
-            // case 7: exercise7(); break;
-            case 8: exercise8(); break;
-            case 9: exercise9(); break;
-        	default: System.out.println("unknown exercise");
 
-            
+        switch (number) {
+            case 1:
+                exercise1();
+                break;
+            case 2:
+                exercise2();
+                break;
+            case 3:
+                exercise3();
+                break;
+            case 4:
+                exercise4();
+                break;
+            case 5:
+                exercise5();
+                break;
+            case 6:
+                exercise6();
+                break;
+            case 7:
+                exercise7();
+                break;
+            case 8:
+                exercise8();
+                break;
+            case 9:
+                exercise9();
+                break;
+
+            case 10:
+                exercise10();
+                break;
+
+            case 11:
+                exercise11();
+                break;
+
+            default:
+                System.out.println("unknown exercise");
+
         }
     }
 
     public void exercise1() {
         System.out.println("Enter upper bound:");
         int userInt = reader.nextInt();
-        for (int i=0; i<=userInt; i++) {
+        for (int i = 0; i <= userInt; i++) {
             System.out.println(i);
         }
     }
@@ -48,7 +75,7 @@ public class W05Exercise1 {
             System.out.println(i);
             i = i + 1;
         }
-    
+
     }
 
     public void exercise3() {
@@ -56,10 +83,10 @@ public class W05Exercise1 {
         int userLowerInt = reader.nextInt();
         System.out.println("Enter upper bound:");
         int userUpperInt = reader.nextInt();
-        for (int i=userLowerInt; i<=userUpperInt; i++) {
+        for (int i = userLowerInt; i <= userUpperInt; i++) {
             System.out.println(i);
         }
-        
+
     }
 
     public void exercise4() {
@@ -67,7 +94,7 @@ public class W05Exercise1 {
         int userLowerInt = reader.nextInt();
         System.out.println("Enter upper bound:");
         int userUpperInt = reader.nextInt();
-        for (int i=userLowerInt; i<=userUpperInt; i+=2) {
+        for (int i = userLowerInt; i <= userUpperInt; i += 2) {
             System.out.println(i);
         }
     }
@@ -77,7 +104,7 @@ public class W05Exercise1 {
         int userLowerInt = reader.nextInt();
         System.out.println("Enter upper bound:");
         int userUpperInt = reader.nextInt();
-        for (int i=userLowerInt; i<=userUpperInt; i*=10) {
+        for (int i = userLowerInt; i <= userUpperInt; i *= 10) {
             System.out.println(i);
         }
     }
@@ -87,30 +114,30 @@ public class W05Exercise1 {
         int userLowerInt = reader.nextInt();
         System.out.println("Enter upper bound:");
         int userUpperInt = reader.nextInt();
-        for (int i=userUpperInt; i>=userLowerInt; i-=3) {
+        for (int i = userUpperInt; i >= userLowerInt; i -= 3) {
             System.out.println(i);
         }
     }
 
-    // public void exercise7() {
-    //     System.out.println("Enter sequence length:");
-    //     int userSeq = reader.nextInt();
-    //     for (int i=0; i<=userSeq; i++) {
-    //         if (i < userSeq) {
-    //             println($("x").repeat(i) + ", ");
-    //         }
-    //         else {
-    //             println($("x").repeat(i));
-    //         }
-            
-    //     }
-    // }
+    public void exercise7() {
+        System.out.println("Enter sequence length:");
+        int userSeq = reader.nextInt();
+        String star = "x";
+        for (int i = 0; i < userSeq; i++) {
+            if (i < userSeq - 1) {
+                System.out.print(star + ", ");
+                star = star + "x";
+            } else {
+                System.out.print(star);
+            }
+        }
+    }
 
     public void exercise8() {
         System.out.println("Enter number:");
         int userInt = reader.nextInt();
         while (userInt != -1) {
-            
+
             for (int i = 0; i < 2; i++) {
                 System.out.println(userInt);
             }
@@ -133,10 +160,31 @@ public class W05Exercise1 {
     }
 
     public void exercise10() {
-    
+        System.out.println("Enter name:");
+        String userInput = reader.next();
+        for (int i = 0; i < userInput.length(); i++) {
+            System.out.println(userInput.charAt(i));
+        }
     }
 
     public void exercise11() {
-    
+
+        System.out.println("Enter number:");
+        int userSeq = reader.nextInt();
+        String star = "x ";
+        for (int i = 0; i < userSeq; i++) {
+            System.out.println(star);
+            star = star + "x ";
+        }
+
+        // for (int i = 1; i <= userSeq; i++) {
+        // userSeq = userSeq - 1;
+        // int endIndex = 2 * userSeq;
+        // star = star.substring(0, endIndex);
+        // System.out.println(star);
+
+        // }
+
     }
+
 }
