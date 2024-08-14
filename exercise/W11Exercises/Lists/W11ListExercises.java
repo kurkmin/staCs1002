c436fef8594b58638d278a5fa3e7b58e99e30b88
Sumin Oh <kurkmin@gmail.com>
2024-08-14 20:15:37 +0100

    week 11 exercise-list done except 7

--- exercise/W11Exercises/Lists/W11ListExercises.java
+++ exercise/W11Exercises/Lists/W11ListExercises.java
@@ -4,72 +4,143 @@ import java.util.Scanner;
 
 public class W11ListExercises {
 
-    Scanner reader = new Scanner(System.in);
+	Scanner reader = new Scanner(System.in);
 
-    public static void main(String[] args) {
-    
+	public static void main(String[] args) {
+
 		W11ListExercises arrays = new W11ListExercises();
 		arrays.runExercises();
-    }
-    
-    public void runExercises() {
+	}
 
-        System.out.println("Enter exercise number:");
-        int number = reader.nextInt();
-        
-    	switch (number) {
-    	
-    		case(1):
-    			exercise1();
-    			break;
-    		case(2):
-    			exercise2();
-    			break;
-    		case(3):
-    			exercise3();
-    			break;
-    		case(4):
-    			exercise4();
-    			break;
-    		case(5):
-    			exercise5();
-    			break;
-    		case(6):
-    			exercise6();
-    			break;
-    		case(7):
-    			exercise7();
-    			break;
-    		default:
-    			System.out.println("unknown exercise");
-    	}
-    }
+	public void runExercises() {
 
-    public void exercise1() {
-    
-    }
+		System.out.println("Enter exercise number:");
+		int number = reader.nextInt();
 
-    public void exercise2() {
-    
-    }
+		switch (number) {
 
-    public void exercise3() {
-    
-    }
+			case (1):
+				exercise1();
+				break;
+			case (2):
+				exercise2();
+				break;
+			case (3):
+				exercise3();
+				break;
+			case (4):
+				exercise4();
+				break;
+			case (5):
+				exercise5();
+				break;
+			case (6):
+				exercise6();
+				break;
+			case (7):
+				exercise7();
+				break;
+			default:
+				System.out.println("unknown exercise");
+		}
+	}
 
-    public void exercise4() {
-    
-    }
+	public void exercise1() {
+		List<Integer> myList = new ArrayList<Integer>();
+		System.out.println("Enter number of elements: ");
+		int numElements = reader.nextInt();
+		for (int i = 0; i < numElements; i++) {
+			System.out.println("Enter element: ");
+			int element = reader.nextInt();
+			myList.add(element);
+		}
 
-    public void exercise5() {
-    
-    }
+		System.out.println("Elements: ");
+		for (int i = 0; i < myList.size(); i++) {
+			System.out.println(myList.get(i));
+		}
+	}
 
-    public void exercise6() {
-    
-    }
+	public List<Integer> getList() {
+		// create a list and its size based on user input
+		List<Integer> myList = new ArrayList<Integer>();
+		System.out.println("Enter number of elements: ");
+		int numElements = reader.nextInt();
+		// adds elements
+		for (int i = 0; i < numElements; i++) {
+			System.out.println("Enter element: ");
+			int element = reader.nextInt();
+			myList.add(element);
+		}
+		return myList;
+	}
 
-    public void exercise7() {
-    
-    }
+	public void exercise2() {
+		List<Integer> myList = getList();
+		System.out.println("Elements: ");
+		for (int i = 0; i < myList.size(); i++) {
+			System.out.println(myList.get(i));
+		}
+	}
+
+	// return the maximum int in the list
+	public void exercise3() {
+		List<Integer> myList = getList();
+		int max = 0;
+		for (int i = 0; i < myList.size(); i++) {
+			if (myList.get(i) > max) {
+				max = myList.get(i);
+			}
+		}
+		System.out.println("the maximum integer in the list is: " + max);
+	}
+
+	public void exercise4() {
+		List<Integer> myList = getList();
+		int total = 0;
+		for (int i = 0; i < myList.size(); i++) {
+			total += myList.get(i);
+		}
+		double mean = total / myList.size();
+		System.out.println(mean);
+	}
+
+	public void exercise5() {
+		List<Integer> myList = getList();
+		System.out.println("Enter element to search for");
+		int userElement = reader.nextInt();
+		boolean found = false;
+		for (int i = 0; i < myList.size(); i++) {
+			if (myList.get(i) == userElement) {
+				System.out.println(myList.indexOf(myList.get(i)));
+				found = true;
+			}
+		}
+
+		if (!found) {
+			System.out.println("-1");
+		}
+	}
+
+	public void exercise6() {
+		List<Integer> myList = getList();
+		System.out.println("Enter element to search for");
+		int userElement = reader.nextInt();
+		boolean found = false;
+		for (int i = 0; i < myList.size(); i++) {
+			if (myList.get(i) == userElement) {
+				System.out.println(myList.lastIndexOf(myList.get(i)));
+			}
+		}
+
+		if (found) {
+			System.out.println("-1");
+			found = true;
+		}
+	}
+
+	public void exercise7() {
+		// should be added
+	}
+
 }
